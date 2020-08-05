7ef8836dbbcd638f1bbdb22ca0639e35fb75fdaa
xtine <xtineburrough@gmail.com>
2020-08-05 15:10:02 -0500

    added pre-recorded messages and links to options in the prompts file

--- playdata.py
+++ playdata.py
@@ -1,14 +1,14 @@
 ### plays in "/'"
-introFile = ["https://burgundy-toad-2613.twil.io/assets/INTRODUCTION-sean.mp3"]
+introFile = ["https://burgundy-toad-2613.twil.io/assets/INTRODUCTION-sean.mp3","https://burgundy-toad-2613.twil.io/assets/INTRODUCTION-leticia.wav"]
 ## Old welcome file list for Message for a Revolution: "https://burgundy-toad-2613.twil.io/assets/WELCOME-xtine.wav", "https://burgundy-toad-2613.twil.io/assets/WELCOME-xtine.wav"
 ## New welcome file list below for Needful Message; will change after August 6, 2020
-welcomeFile = ["https://burgundy-toad-2613.twil.io/assets/INTRODUCTION-needful-lets.wav", "https://burgundy-toad-2613.twil.io/assets/INTRODUCTION-needful-mumu.wav"]
+welcomeFile = ["https://burgundy-toad-2613.twil.io/assets/INTRODUCTION-needful-lets.wav", "https://burgundy-toad-2613.twil.io/assets/INTRODUCTION-needful-mumu.wav","https://burgundy-toad-2613.twil.io/assets/elmira-xtine-intro-prompt.wav","https://burgundy-toad-2613.twil.io/assets/elmira-cynthia-intro-needful.wav"]
 
 ### plays in "/record"
 recordFile = ["https://burgundy-toad-2613.twil.io/assets/RECORDING-sabrina.mp3"]
 
 # plays in "/recordReview" if no recording is found
-errorFile = ["https://burgundy-toad-2613.twil.io/assets/ERROR-leticia.mp3"]
+errorFile = ["https://burgundy-toad-2613.twil.io/assets/ERROR-leticia.mp3","https://burgundy-toad-2613.twil.io/assets/ERROR-xtine.wav","https://burgundy-toad-2613.twil.io/assets/ERROR-elmira.wav"]
 
 #plays in "/recordReview", prompt to save or toss recording
 reviewFile = ["https://burgundy-toad-2613.twil.io/assets/REVIEWING-cynthia.mp3"]
@@ -17,9 +17,10 @@ reviewFile = ["https://burgundy-toad-2613.twil.io/assets/REVIEWING-cynthia.mp3"]
 choiceFile = ["https://burgundy-toad-2613.twil.io/assets/LISTENINGTOMESSAGES-elmira.mp3"]
 
 # plays in "/listen", offering option to listen to more messages or record messages
-listenFile = ["https://burgundy-toad-2613.twil.io/assets/ABOUTUS2-maedeh.mp3"]
+listenFile = ["https://burgundy-toad-2613.twil.io/assets/ABOUTUS2-maedeh.mp3","https://burgundy-toad-2613.twil.io/assets/sean-to-listen-to-another-message.wav"]
 
 # plays in "/aboutus", as intro to who LabSynthE is
 aboutusFile = ["https://burgundy-toad-2613.twil.io/assets/ABOUTUS-xtine.mp3"]
 # plays in "/aboutus", as prompt to make a choice
 aboutchoiceFile = ["https://burgundy-toad-2613.twil.io/assets/ABOUTUS2-maedeh.mp3"]
+### goodbye file "https://burgundy-toad-2613.twil.io/assets/leticia-thanksforcalling-bye.wav"
